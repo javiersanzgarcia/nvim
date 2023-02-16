15622f6ad2b8ab0a772d559a99c0ad21b48fc555
Javier Sanz García <31390647+javiersanzgarcia@users.noreply.github.com>
2023-02-16 11:31:36 +0100

    Apply transparent background and storm style

--- lua/config/colorscheme.lua
+++ lua/config/colorscheme.lua
@@ -9,20 +9,19 @@ local c = require("tokyonight.colors").setup()
 -- │ Setup Colorscheme                                        │
 -- ╰──────────────────────────────────────────────────────────╯
 tokyonight.setup({
-  style = "night",
-
-  transparent = false, -- Enable this to disable setting the background color
+  style = "storm", -- The theme comes in three styles, `storm`, `moon`, a darker variant `night` and `day`
+  transparent = true, -- Enable this to disable setting the background color
   terminal_colors = true, -- Configure the colors used when opening a `:terminal` in Neovim
   styles = {
-    -- Style to be applied to different syntax groups
-    -- Value is any valid attr-list value `:help attr-list`
-    comments = "italic",
-    keywords = "italic",
-    functions = "NONE",
-    variables = "NONE",
-    -- Background styles. Can be "dark", "transparent" or "normal"
-    sidebars = "dark", -- style for sidebars, see below
-    floats = "dark", -- style for floating windows
+      -- Style to be applied to different syntax groups
+      -- Value is any valid attr-list value `:help attr-list`
+      comments = "italic",
+      keywords = "italic",
+      functions = "NONE",
+      variables = "NONE",
+      -- Background styles. Can be "dark", "transparent" or "normal"
+      sidebars = "transparent", -- style for sidebars, see below
+      floats = "transparent" -- style for floating windows
   },
   sidebars = { "qf", "help" }, -- Set a darker background on sidebar-like windows. For example: `["qf", "vista_kind", "terminal", "packer"]`
   day_brightness = 0.3, -- Adjusts the brightness of the colors of the **Day** style. Number between 0 and 1, from dull to vibrant colors
