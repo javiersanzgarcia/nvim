719270c6b769b59129dedb3e4d5244218da44250
javiersanzgarcia <pracker@gmail.com>
2023-03-04 13:46:23 +0100

    fzf plugin deactivate (is not working in Linux)

--- lua/config/plugins.lua
+++ lua/config/plugins.lua
@@ -41,7 +41,7 @@ return {
         config = function() require("plugins.telescope") end,
         dependencies = {
             {"nvim-lua/popup.nvim"}, {"nvim-lua/plenary.nvim"},
-            {"nvim-telescope/telescope-fzf-native.nvim", build = "make"},
+            -- {"nvim-telescope/telescope-fzf-native.nvim", build = "make"},
             {"cljoly/telescope-repo.nvim"}
         }
     }, {"nvim-pack/nvim-spectre"}, {
